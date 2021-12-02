4f2a43e9352d7e492ec0fc662463c677246d7ee7
Adam Baumgartner <adambaumgartner@gmail.com>
2021-12-02 13:45:38 -0500

    day 2 draft

--- day2/day2.cpp
+++ day2/day2.cpp
@@ -1,5 +1,27 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
 
-int main () {
-    std::cout << "Hello World" << "\n";
+using std::cout;
+using std::ifstream;
+using std::string;
+using std::vector;
+using std::istringstream;
+
+
+int main () { 
+  string path = "input.txt";
+  ifstream myfile(path);
+  if(myfile){
+    string line;
+    while(getline(myfile, line)){
+      string direction;
+      int distance
+      istringstream stream(line);
+      while(stream >> n >> c){
+        cout << std::format("Direction: {}, Distance: {}", direction, distance);
+      }
+    }
+  }
 }
